57b06e87b2209350e1a5831e8fce8ab93757dc34
MatthiasMargot <matthiasmargot@hotmail.com>
2019-07-31 18:22:57 +0300

    Add algo content

---
+++ algo.js
@@ -0,0 +1,20 @@
+/*
+A theatre employee wants a list of all the seats from the main room.
+
+In this room, the seats are organised as follows:
+    - There are 26 columns of seats numbered from "1" to "26"
+    - Each column contains 100 seats numbered from "1" to "100"
+
+In the end, the list should look like this:
+1-1
+1-2
+1-3
+.
+.
+.
+26-98
+26-99
+26-100
+
+Create a function that returns an array containing all of the seat numbers.
+*/
