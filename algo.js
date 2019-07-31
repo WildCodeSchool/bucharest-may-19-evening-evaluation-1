363ef0fc43884c5119cdb1eb863031662e3a61ee
Lavinia Chiburte <lavinia.chiburte@gmail.com>
2019-07-31 19:45:28 +0300

    Adds exercise 1 - Function JS

--- algo.js
+++ algo.js
@@ -18,3 +18,36 @@ In the end, the list should look like this:
 
 Create a function that returns an array containing all of the seat numbers.
 */
+
+// function columns(col) {
+// 	let columnsArray = [];
+
+// 	for (let i = 1; i <= col; i++) {
+// 		columnsArray.push(i);
+// 	}
+// 	return columnsArray;
+// }
+// console.log(columns(26));
+
+// function rows(row) {
+// 	let rowsArray = [];
+
+// 	for (let i = 1; i <= row; i++) {
+// 		rowsArray.push(i);
+// 	}
+// 	return rowsArray;
+// }
+
+// console.log(rows(100));
+
+function setSeatsNumbers(col, rows) {
+	let seatsNumber = [];
+	for (let i = 1; i <= col; i++) {
+		for (var j = 1; j <= rows; j++) {
+			seatsNumber.push(i + '-' + j);
+		}
+	}
+	return seatsNumber;
+}
+
+console.log(setSeatsNumbers(26, 100));
